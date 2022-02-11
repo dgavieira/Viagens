bfcc10dfdbf6d3cab596d5c99aad94bbcdc657ac
Diego Vieira <dgavieira@users.noreply.github.com>
2022-02-11 15:48:15 -0400

    Initial Commit

---
+++ app/src/main/java/com/dgavieira/viagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package com.dgavieira.viagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dgavieira.viagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
