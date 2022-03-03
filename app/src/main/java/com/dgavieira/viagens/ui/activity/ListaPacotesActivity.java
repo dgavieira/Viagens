1e790f0d3f3f74a4a093dde14340702cd7e78b5f
Diego Vieira <dgavieira@users.noreply.github.com>
2022-03-03 16:02:28 -0400

    Implementando Lista Personalizada

--- app/src/main/java/com/dgavieira/viagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/com/dgavieira/viagens/ui/activity/ListaPacotesActivity.java
@@ -3,8 +3,15 @@ package com.dgavieira.viagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 import com.dgavieira.viagens.R;
+import com.dgavieira.viagens.dao.PacoteDAO;
+import com.dgavieira.viagens.model.Pacote;
+import com.dgavieira.viagens.ui.adapter.ListaPacotesAdapter;
+
+import java.util.List;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -12,5 +19,13 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+        List<Pacote> pacotes = new PacoteDAO().lista();
+
+        listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
+
+
+
     }
 }
