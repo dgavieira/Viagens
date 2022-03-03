66bcb2330ff5e1ecc91f2bd01c7c014be067ae3f
Diego Vieira <dgavieira@users.noreply.github.com>
2022-03-03 17:13:52 -0400

    Refatorando Código Criando Classes Utilitárias

--- app/src/main/java/com/dgavieira/viagens/ui/adapter/ListaPacotesAdapter.java
+++ app/src/main/java/com/dgavieira/viagens/ui/adapter/ListaPacotesAdapter.java
@@ -1,7 +1,6 @@
 package com.dgavieira.viagens.ui.adapter;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,13 +11,16 @@ import android.widget.TextView;
 
 import com.dgavieira.viagens.R;
 import com.dgavieira.viagens.model.Pacote;
+import com.dgavieira.viagens.util.DiasUtil;
+import com.dgavieira.viagens.util.MoedaUtil;
+import com.dgavieira.viagens.util.ResourceUtil;
 
 import java.util.List;
 
 public class ListaPacotesAdapter extends BaseAdapter {
 
     private final List<Pacote> pacotes;
-    private Context context;
+    private final Context context;
 
     public ListaPacotesAdapter(List<Pacote> pacotes, Context context) {
         this.pacotes = pacotes;
@@ -47,22 +49,37 @@ public class ListaPacotesAdapter extends BaseAdapter {
 
         Pacote pacote = pacotes.get(posicao);
 
-        TextView local = viewCriada.findViewById(R.id.item_pacote_local);
-        local.setText(pacote.getLocal());
-
-        ImageView imagem = viewCriada.findViewById(R.id.item_pacote_imagem);
-        Resources resources = context.getResources();
-        int idDoDrawable = resources.getIdentifier(pacote.getImagem(), "drawable", context.getPackageName());
-        Drawable drawableImagemPacote = resources.getDrawable(idDoDrawable);
-        imagem.setImageDrawable(drawableImagemPacote);
-
-        TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
-        dias.setText(pacote.getDias() + "dias");
-
-        TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
-        preco.setText(pacote.getPreco().toString());
-
+        mostraLocal(viewCriada, pacote);
+        mostraImagem(viewCriada, pacote);
+        mostraDias(viewCriada, pacote);
+        mostraPreco(viewCriada, pacote);
 
         return viewCriada;
+    }
+
+    private void mostraPreco(View viewCriada, Pacote pacote) {
+        TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
+        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacote.getPreco());
+        preco.setText(moedaBrasileira);
+    }
+
+
+    private void mostraDias(View viewCriada, Pacote pacote) {
+        TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
+        String diasEmTexto = DiasUtil.formataEmTexto(pacote.getDias());
+        dias.setText(diasEmTexto);
+    }
+
+
+    private void mostraImagem(View viewCriada, Pacote pacote) {
+        ImageView imagem = viewCriada.findViewById(R.id.item_pacote_imagem);
+        Drawable drawableImagemPacote = ResourceUtil.devolveDrawable(context, pacote.getImagem());
+        imagem.setImageDrawable(drawableImagemPacote);
+    }
+
+
+    private void mostraLocal(View viewCriada, Pacote pacote) {
+        TextView local = viewCriada.findViewById(R.id.item_pacote_local);
+        local.setText(pacote.getLocal());
     }
 }
---
+++ app/src/main/java/com/dgavieira/viagens/util/DiasUtil.java
@@ -0,0 +1,19 @@
+package com.dgavieira.viagens.util;
+
+import androidx.annotation.NonNull;
+
+import com.dgavieira.viagens.model.Pacote;
+
+public class DiasUtil {
+
+    public static final String PLURAL = " dias";
+    public static final String SINGULAR = " dia";
+
+    @NonNull
+    public static String formataEmTexto(int quantidadeDeDias) {
+        if (quantidadeDeDias > 1) {
+            return quantidadeDeDias + PLURAL;
+        }
+        return quantidadeDeDias + SINGULAR;
+    }
+}
