0f18531ebd5c04b3d3d60d0d474b299f6a5a2099
Diego Vieira <dgavieira@users.noreply.github.com>
2022-03-12 13:24:40 -0400

    Criando a tela do resumo do pacote

---
+++ app/src/main/java/com/dgavieira/viagens/ui/activity/ResumoPacoteActivity.java
@@ -0,0 +1,14 @@
+package com.dgavieira.viagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResumoPacoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_pacote);
+    }
+}
