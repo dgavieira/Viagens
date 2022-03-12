8358e7827babd92e1dcfcfb5d96447a6be9c3282
Diego Vieira <dgavieira@users.noreply.github.com>
2022-03-12 18:15:32 -0400

    Refatorando código

--- app/src/main/java/com/dgavieira/viagens/ui/activity/ResumoPacoteActivity.java
+++ app/src/main/java/com/dgavieira/viagens/ui/activity/ResumoPacoteActivity.java
@@ -1,61 +1,69 @@
 package com.dgavieira.viagens.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.dgavieira.viagens.R;
 import com.dgavieira.viagens.model.Pacote;
+import com.dgavieira.viagens.util.DataUtil;
 import com.dgavieira.viagens.util.DiasUtil;
 import com.dgavieira.viagens.util.MoedaUtil;
 import com.dgavieira.viagens.util.ResourceUtil;
 
 import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 public class ResumoPacoteActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Resumo do Pacote";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("Resumo do Pacote");
         setContentView(R.layout.activity_resumo_pacote);
+
+        setTitle(TITULO_APPBAR);
 
         Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp",
                 2, new BigDecimal("243.99"));
 
-        TextView local = findViewById(R.id.resumo_pacote_local);
-        local.setText(pacoteSaoPaulo.getLocal());
+        mostraLocal(pacoteSaoPaulo);
+        mostraImagem(pacoteSaoPaulo);
+        mostraDias(pacoteSaoPaulo);
+        mostraPreco(pacoteSaoPaulo);
+        mostraData(pacoteSaoPaulo);
+    }
 
-        ImageView imagem = findViewById(R.id.resumo_pacote_imagem);
-        Drawable drawableDoPacote = ResourceUtil.devolveDrawable(this, pacoteSaoPaulo.getImagem());
-        imagem.setImageDrawable(drawableDoPacote);
-
-        TextView dias = findViewById(R.id.resumo_pacote_dias);
-        String diasEmTexto = DiasUtil.formataEmTexto(pacoteSaoPaulo.getDias());
-        dias.setText(diasEmTexto);
-
-        TextView preco = findViewById(R.id.resumo_pacote_preco_final);
-        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacoteSaoPaulo.getPreco());
-        preco.setText(moedaBrasileira);
-
+    private void mostraData(Pacote pacote) {
         TextView data = findViewById(R.id.resumo_pacote_data);
-        Calendar dataIda = Calendar.getInstance();
-        Calendar dataVolta = Calendar.getInstance();
-        dataVolta.add(Calendar.DATE, pacoteSaoPaulo.getDias());
-        SimpleDateFormat formatoBrasileiro = new SimpleDateFormat("dd/MM");
-        String dataFormatadaIda = formatoBrasileiro.format(dataIda.getTime());
-        String dataFormatadaVolta = formatoBrasileiro.format(dataVolta.getTime());
-        String dataFormatadaDaViagem = dataFormatadaIda + " - "
-                + dataFormatadaVolta + " de "
-                + dataVolta.get(Calendar.YEAR);
+        String dataFormatadaDaViagem = DataUtil.periodoEmTexto(pacote.getDias());
         data.setText(dataFormatadaDaViagem);
+    }
 
 
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.resumo_pacote_preco_final);
+        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacote.getPreco());
+        preco.setText(moedaBrasileira);
+    }
+
+    private void mostraDias(Pacote pacote) {
+        TextView dias = findViewById(R.id.resumo_pacote_dias);
+        String diasEmTexto = DiasUtil.formataEmTexto(pacote.getDias());
+        dias.setText(diasEmTexto);
+    }
+
+    private void mostraImagem(Pacote pacote) {
+        ImageView imagem = findViewById(R.id.resumo_pacote_imagem);
+        Drawable drawableDoPacote = ResourceUtil.devolveDrawable(this, pacote.getImagem());
+        imagem.setImageDrawable(drawableDoPacote);
+    }
+
+    private void mostraLocal(Pacote pacote) {
+        TextView local = findViewById(R.id.resumo_pacote_local);
+        local.setText(pacote.getLocal());
     }
 }
